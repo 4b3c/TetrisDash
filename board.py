d41c3fac3e4ee5d129d5876182388cdb606a3db8
Abram <58718191+4b3c@users.noreply.github.com>
2024-06-06 11:42:18 -0700

    made valid_position return a bool, added clearing lines

--- board.py
+++ board.py
@@ -13,8 +13,10 @@ class Board:
 		self.size = 30
 		self.pos = (100, 100)
 		self.shapes = []
-		self.spawn_piece()
 		self.full_cells = {}
+		self.game_over = False
+		self.score = 0
+		self.spawn_piece()
 
 	# Creates a new piece at the top of the grid
 	def spawn_piece(self):
@@ -23,6 +25,9 @@ class Board:
 			random.shuffle(self.shapes)
 		self.piece = Shape(self.shapes.pop(0))
 		self.piece_pos = [2, 0]
+		if not self.valid_pos():
+			self.game_over = True
+		self.score += 10
 
 	# Fills in a square in the grid by coordinate
 	def draw_square(self, surf, color, pos):
@@ -39,42 +44,64 @@ class Board:
 		for i in range(self.pos[1], (height * self.size) + self.pos[1] + 1, self.size):
 			pygame.draw.line(surf, self.color, (self.pos[0], i), (self.pos[0] + (width * self.size), i))
 
+		for coord in self.full_cells:
+			self.draw_square(surf, self.full_cells[coord], coord)
+
 	# Uses the function for filling a single cell to draw the shape of the moving piece
 	def draw_piece(self, surf):
 		for coord in self.piece.squares():
 			self.draw_square(surf, self.piece.color, (coord[0] + self.piece_pos[0], coord[1] + self.piece_pos[1]))
 
-	# Figues out how far to move a piece back if it is not in a valid position
-	def valid_pos(self, x_or_y, edge) -> int:
-		moveback = 0
+	# Determines if a pice is in a valid position
+	def valid_pos(self) -> bool:
 		for coord in self.piece.squares():
-			abs_pos = coord[x_or_y] + self.piece_pos[x_or_y]
-			if abs_pos < 0 and (0 - abs_pos) > moveback:
-				moveback = 0 - abs_pos
-			elif abs_pos > edge and (edge - abs_pos) < moveback:
-				moveback = edge - abs_pos
-			elif coord in self.full_cells:
-				moveback = -1
-
-		return moveback
+			abs_x = coord[0] + self.piece_pos[0]
+			abs_y = coord[1] + self.piece_pos[1]
+			if abs_x < 0 or abs_x > 9: return False
+			elif abs_y > 19: return False
+			elif (abs_x, abs_y) in self.full_cells: return False
+		return True
 	
-	def move_x(self, dx):
+	def move_x(self, dx) -> None:
 		self.piece_pos[0] += dx
-		self.piece_pos[0] += self.valid_pos(0, 9)
+		if not self.valid_pos():
+			self.piece_pos[0] -= dx
 
-	def move_y(self, dy):
+	def move_y(self, dy) -> None:
 		self.piece_pos[1] += dy
-		moveback = self.valid_pos(1, 19)
-		if moveback != 0:
-			self.piece_pos[1] += moveback
+		if not self.valid_pos():
+			self.piece_pos[1] -= dy
+			for coord in self.piece.squares():
+				self.full_cells[(coord[0] + self.piece_pos[0], coord[1] + self.piece_pos[1])] = self.piece.color
+				self.clear_lines()
+			self.spawn_piece()
 
 	def rotate(self, direction) -> None:
 		self.piece.rotate(direction)
-		self.piece_pos[0] += self.valid_pos(0, 9)
-		self.piece_pos[1] += self.valid_pos(1, 19)
+		if not self.valid_pos():
+			self.piece.rotate(-direction)
 
-	def update(self, surf):
+	def update(self, surf) -> None:
 		self.draw_grid(surf)
 		self.draw_piece(surf)
 
-	
+	def clear_line(self, i) -> bool:
+		for j in range(width):
+			if (j, i) not in self.full_cells:
+				return False
+		for j in range(width):
+			self.full_cells.pop((j, i))
+
+		for k in range(i, 0, -1):
+			for j in range(width):
+				if (j, k - 1) in self.full_cells:
+					self.full_cells[(j, k)] = self.full_cells[(j, k - 1)]
+					self.full_cells.pop((j, k - 1))
+
+
+	def clear_lines(self) -> None:
+		score_multiplier = 1
+		for i in range(height):
+			if self.clear_line(i):
+				self.score += 100 * score_multiplier
+				score_multiplier += 1
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pygame
 import time
 import board
-import shape
 pygame.init()
 
 
@@ -10,7 +9,18 @@ pause = 0.2
 
 game_board = board.Board()
 last_time = time.time()
-
+last_time_key_pressed = time.time()
+pressed_keys = {
+	pygame.K_w: False,
+	pygame.K_a: False,
+	pygame.K_s: False,
+	pygame.K_d: False,
+	pygame.K_UP: False,
+	pygame.K_LEFT: False,
+	pygame.K_DOWN: False,
+	pygame.K_RIGHT: False,
+	pygame.K_SPACE: False
+}
 
 while True:
 	window.fill((20, 30, 40))
@@ -19,16 +29,37 @@ while True:
 
 	if (time.time() - last_time) > pause:
 		game_board.move_y(1)
+		if game_board.game_over:
+			game_board = board.Board()
 		last_time = time.time()
+
+	if (time.time() - last_time_key_pressed) > 0.03:
+		pressed_keys = {
+			pygame.K_w: False,
+			pygame.K_a: False,
+			pygame.K_s: False,
+			pygame.K_d: False,
+			pygame.K_UP: False,
+			pygame.K_LEFT: False,
+			pygame.K_DOWN: False,
+			pygame.K_RIGHT: False,
+			pygame.K_SPACE: False
+		}
+		last_time_key_pressed = time.time()
+
+	print(pressed_keys[pygame.K_SPACE])
+	pressed = pygame.key.get_pressed()
+	if pressed[pygame.K_SPACE] and not pressed_keys[pygame.K_SPACE]:
+		if game_board.valid_pos():
+			game_board.move_y(1)
+			pressed_keys[pygame.K_SPACE] = True
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			quit()
-		elif event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+		elif event.type == pygame.KEYDOWN and (event.key == pygame.K_UP or event.key == pygame.K_w):
 			game_board.rotate(1)
-		elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
-			game_board.rotate(-1)
-		elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+		elif event.type == pygame.KEYDOWN and (event.key == pygame.K_RIGHT or event.key == pygame.K_d):
 			game_board.move_x(1)
-		elif event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
+		elif event.type == pygame.KEYDOWN and (event.key == pygame.K_LEFT or event.key == pygame.K_a):
 			game_board.move_x(-1)
